209da5717909fdaf3fb209cb3315aa004ff5ab0f
butterfly <guillaume.cozic@gmail.com>
2020-06-20 11:35:16 +0200

    fix docker script install

--- install.sh
+++ install.sh
@@ -4,11 +4,11 @@ docker-compose up -d
 
 cp ".env.example" ".env"
 
-docker exec php_neayi sh -c "chmod -R 777 storage/"
-docker exec php_neayi sh -c "composer install"
-docker exec php_neayi sh -c "php artisan migrate"
-docker exec php_neayi sh -c "vendor/bin/phpunit tests/Unit"
-docker exec php_neayi sh -c "vendor/bin/phpunit tests/Unit/ -c phpunit-ti-domain-sql.xml"
+docker exec -it neayi_php sh -c "chmod -R 777 storage/"
+docker exec -it neayi_php sh -c "composer install"
+docker exec -it neayi_php sh -c "php artisan migrate"
+docker exec -it neayi_php sh -c "vendor/bin/phpunit tests/Unit"
+docker exec -it neayi_php sh -c "vendor/bin/phpunit tests/Unit/ -c phpunit-ti-domain-sql.xml"
 
 echo ""
 echo "You should add to your hosts : 127.0.0.1 dev.core.tripleperformance.com"
