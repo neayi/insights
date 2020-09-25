fc03032544c936fa792267e7755e82a64c6a64e4
Guillaume COZIC <guillaume.cozic@gmail.com>
2020-09-25 13:39:55 +0200

    refacto, remove duplication

--- app/Src/UseCases/Domain/Invitation/RespondInvitationToAnOrganization.php
+++ app/Src/UseCases/Domain/Invitation/RespondInvitationToAnOrganization.php
@@ -4,9 +4,11 @@
 namespace App\Src\UseCases\Domain\Invitation;
 
 
+use App\Src\UseCases\Domain\Organization;
 use App\Src\UseCases\Domain\Ports\InvitationRepository;
 use App\Src\UseCases\Domain\Ports\OrganizationRepository;
 use App\Src\UseCases\Domain\Ports\UserRepository;
+use App\Src\UseCases\Domain\User;
 use App\Src\UseCases\Infra\Gateway\Auth\AuthGateway;
 
 class RespondInvitationToAnOrganization
@@ -32,10 +34,8 @@ class RespondInvitationToAnOrganization
     public function respond(string $hash)
     {
         $invitation = $this->invitationRepository->getByHash($hash);
-
         list($organizationId, $email, $firstname, $lastname) = $invitation->data();
         $user = $this->userRepository->getByEmail($email);
-
         $organizationToJoin = $this->organizationRepository->get($organizationId);
 
         $currentUser = $this->authGateway->current();
@@ -44,53 +44,83 @@ class RespondInvitationToAnOrganization
             $action .= !isset($user) ? '-register' : '-login';
 
             if($action == 'logout-login') {
-                return [
-                    'action' => $action,
-                    'organization_to_join' => $organizationToJoin,
-                ];
+                return $this->badUserLoginToAcceptAction($action, $organizationToJoin);
             }
-
-            return [
-                'action' => $action,
-                'organization_to_join' => $organizationToJoin,
-                'user' => [
-                    'email' => $email,
-                    'firstname' => $firstname,
-                    'lastname' => $lastname,
-                ]
-            ];
+            return $this->returnLogoutAction($action, $organizationToJoin, $email, $firstname, $lastname);
         }
 
 
         if(!isset($user)){
-            return [
-                'action' => 'register',
-                'organization_id' => $organizationId,
-                'user' => [
-                    'email' => $email,
-                    'firstname' => $firstname,
-                    'lastname' => $lastname,
-                ]
-            ];
+            return $this->returnRegisterAction($organizationId, $email, $firstname, $lastname);
         }
 
         $currentUser = $this->authGateway->current();
         if(!isset($currentUser)){
-            return [
-                'action' => 'login',
-                'organization_to_join' => $organizationToJoin,
-            ];
+            return $this->returnLoginAction($organizationToJoin);
         }
 
-        $organization = null;
-        if($user->organizationId() !== null) {
-            $organization = $this->organizationRepository->get($user->organizationId());
-        }
+        return $this->acceptOrDeclineAction($organizationToJoin, $currentUser);
+    }
 
+
+    private function acceptOrDeclineAction(?Organization $organizationToJoin, User $user): array
+    {
+        $organization = $this->getCurrentOrganization($user);
         return [
             'action' => 'accept_or_decline',
             'old_organisation' => $organization,
             'organization_to_join' => $organizationToJoin
         ];
+    }
+
+    private function returnLoginAction(?Organization $organizationToJoin): array
+    {
+        return [
+            'action' => 'login',
+            'organization_to_join' => $organizationToJoin,
+        ];
+    }
+
+    private function returnRegisterAction($organizationId, $email, $firstname, $lastname): array
+    {
+        return [
+            'action' => 'register',
+            'organization_id' => $organizationId,
+            'user' => [
+                'email' => $email,
+                'firstname' => $firstname,
+                'lastname' => $lastname,
+            ]
+        ];
+    }
+
+    private function returnLogoutAction(string $action, ?Organization $organizationToJoin, $email, $firstname, $lastname): array
+    {
+        return [
+            'action' => $action,
+            'organization_to_join' => $organizationToJoin,
+            'user' => [
+                'email' => $email,
+                'firstname' => $firstname,
+                'lastname' => $lastname,
+            ]
+        ];
+    }
+
+    private function badUserLoginToAcceptAction(string $action, ?Organization $organizationToJoin): array
+    {
+        return [
+            'action' => $action,
+            'organization_to_join' => $organizationToJoin,
+        ];
+    }
+
+    private function getCurrentOrganization(User $user)
+    {
+        $organization = null;
+        if ($user->organizationId() !== null) {
+            $organization = $this->organizationRepository->get($user->organizationId());
+        }
+        return $organization;
     }
 }
