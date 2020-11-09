a2b235c66354a954b346b63c9690f8317ce12e09
Guillaume COZIC <guillaume.cozic@gmail.com>
2020-11-09 14:58:39 +0100

    fix twitter service

--- config/services.php
+++ config/services.php
@@ -41,4 +41,10 @@ return [
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
         'redirect' => env('GOOGLE_CALLBACK'),
     ],
+
+    'twitter' => [
+        'client_id' => env('TWITTER_CLIENT_ID'),
+        'client_secret' => env('TWITTER_CLIENT_SECRET'),
+        'redirect' => env('TWITTER_CALLBACK'),
+    ],
 ];
