a6a4be55d3991f6b04d1a9d4f5ebde6760f5aa8b
Guillaume COZIC <guillaume.cozic@gmail.com>
2020-11-14 12:06:10 +0100

    commande pour ajouter le role admin a un user, pour le moment personne a accès au bo

---
+++ app/Console/Commands/Debug/GiveAdminRole.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Console\Commands\Debug;
+
+use App\User;
+use Illuminate\Console\Command;
+
+class GiveAdminRole extends Command
+{
+    protected $signature = 'debug:give-admin-role {email}';
+
+    protected $description = 'Give admin role to the user (email) given';
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function handle()
+    {
+        $email = $this->argument('email');
+        $user = User::where('email', $email)->first();
+        $user->assignRole(['admin']);
+    }
+}
