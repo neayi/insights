3f7a5387c13e6937d4284a27041a67bda7ba675f
Guillaume COZIC <guillaume.cozic@gmail.com>
2020-11-27 13:56:59 +0100

    fix html responsive

--- resources/views/users/wizard-profile/fill-postal-code.blade.php
+++ resources/views/users/wizard-profile/fill-postal-code.blade.php
@@ -7,10 +7,10 @@
 <div class="form-group">
     <label id="state-postal" class="label-big {{$state}} mb-3">J'habite</label>
     <div class="row align-items-center">
-        <div class="col-7">
+        <div class="col-lg-7 col-3">
             <label>Code postal</label>
         </div>
-        <div class="col-5 pl-0">
+        <div class="col-lg-5 col-9">
             <input value="{{old('postal_code')}}" type="text" id="input-postal" name="postal_code" class="form-control" placeholder="">
         </div>
     </div>
--- resources/views/users/wizard-profile/wizard.blade.php
+++ resources/views/users/wizard-profile/wizard.blade.php
@@ -6,17 +6,16 @@
 
 <form class="form" role="form" method="post" action="{{ route('wizard.profile.process') }}">
 @csrf
-    <div class="row cta-fixed-bar fixed-top grey-bg py-3 align-items-center">
-        <div class="col-md-2 text-center">
-            <strong>Présentez-vous</strong>
+    <div class="row cta-fixed-bar fixed-top grey-bg py-3 align-items-center mw-100">
+        <div class="col-lg-3 offset-lg-2 col-md-4">
+            <button type="button" class="btn btn-dark-green text-white px-5 py-2 mr-2 w-100 mb-2 mb-md-0 ml-3">Valider</button>
         </div>
-        <div class="col-md-9 pl-4">
-            <button type="submit" class="btn btn-dark-green text-white px-5 py-2 mr-2">Valider</button>
-            <!--button type="button" class="btn btn-outline-darkgreen text-dark px-5 py-2">Valider et continuer de se présenter</button-->
+        <div class="col-lg-5 col-md-6">
+            <button type="button" class="btn btn-outline-darkgreen text-dark px-5 py-2 w-100 ml-3">Valider et continuer de se présenter</button>
         </div>
     </div>
     <div class="row py-5">
-        <div class="col-md-8 offset-md-2 col-12">
+        <div class="col-lg-8 offset-lg-2 col-12">
             <span class="font-weight-bold text-dark-green text-big d-block mb-4">Merci !</span>
             <p class="mb-4">
                 Pour vous aider à trouver des Retours d’expériences ou des Pratiques pouvant s’appliquer à votre exploitation et aider la communauté à vous comprendre,
@@ -27,7 +26,7 @@
         </div>
     </div>
     <div class="row">
-        <div class="col-lg-2 offset-md-2">
+        <div class="col-lg-2 offset-lg-2">
             @include('users.wizard-profile.select-user-role')
         </div>
         <div class="col-lg-5">
@@ -38,53 +37,55 @@
         </div>
     </div>
     <div class="row">
-        <div class="col-lg-12 offset-md-2">
+        <div class="col-lg-12 offset-lg-2">
             @include('users.wizard-profile.fill-email')
         </div>
     </div>
     <div class="row mt-5 icon-checkboxes" id="select-farming">
-        <div class="col-lg-8 offset-md-2">
+        <div class="col-lg-8 offset-lg-2">
             <div class="form-group">
                 <label class="label-big mb-3">Je suis principalement en</label>
-                <ul class="d-md-flex">
-                    @foreach($farmingTypeMain as $farming)
-                        <li class="mr-4 ">
-                            @include('users.wizard-profile.icon-farming', [
+                <div class="circle-row d-flex flex-wrap">
+                @foreach($farmingTypeMain as $farming)
+                        <div class="circle-item">
+                        @include('users.wizard-profile.icon-farming', [
                                  'uuid' => $farming->uuid,
                                  'code' => $farming->code,
                                  'icon' => $farming->icon,
                              ])
-                        </li>
+                        </div>
                     @endforeach
-                    <li class="mr-4 more" id="more-main-production">
+                    <div class="circle-item" id="more-main-production">
                         <label for="myCheckbox23">
                             <img src="{{asset('icons/More.svg')}}" class="rounded-circle mb-2" />
                         </label>
-                    </li>
-                </ul>
+                    </div>
+                </div>
             </div>
         </div>
         <div class="col-lg-8 offset-md-2" id="second-row-main-production" style="display: none;">
             <div class="form-group">
-                <ul class="d-md-flex">
+                <div class="circle-row d-flex flex-wrap">
                     @foreach($farmingType as $farming)
-                        <li class="mr-4">
+                        <div class="circle-item">
                             @include('users.wizard-profile.icon-farming', [
                                  'uuid' => $farming->uuid,
                                  'code' => $farming->code,
                                  'icon' => $farming->icon,
                              ])
-                        </li>
+                        </div>
                     @endforeach
-                </ul>
+                </div>
             </div>
         </div>
     </div>
     <div class="row">
-        <div class="col-md-8 offset-md-2">
+        <div class="col-lg-3 offset-lg-2 col-md-4">
             <button type="submit" class="btn btn-dark-green text-white px-5 py-2 mr-2">Valider</button>
-            <!--button type="button" class="btn btn-outline-darkgreen text-dark px-5 py-2">Valider et continuer de se présenter</button-->
         </div>
+        <!--div class="col-lg-5 col-md-6">
+            <button type="button" class="btn btn-outline-darkgreen text-dark px-5 py-2 w-100">Valider et continuer de se présenter</button>
+        <div-->
     </div>
 </form>
 @endsection
