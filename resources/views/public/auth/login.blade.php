64e4dd2c39624ba3329a5b22977925e5d6cd6d14
Guillaume COZIC <guillaume.cozic@gmail.com>
2020-12-03 20:27:23 +0100

    fix rechargement page register page

--- resources/views/public/auth/login.blade.php
+++ resources/views/public/auth/login.blade.php
@@ -56,7 +56,7 @@
                                                     </div>
                                                 @endif
                                             </div>
-                                        </div>git checkout
+                                        </div>
                                     </div>
                                     <div class="row">
                                         <div class="col-md-8">
