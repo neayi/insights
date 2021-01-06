03d7f19ad7b10193d242b13aec09488199955495
GuillaumeCozic <guillaume.cozic@gmail.com>
2021-01-06 13:39:50 +0100

    Issue 56 (#57)
    * fix context id
    
    * add title

--- resources/lang/fr/pages.php
+++ resources/lang/fr/pages.php
@@ -1,14 +1,15 @@
 <?php
 
 return [
-  'title_add_organization' => 'Ajouter une organisation',
-  'title_edit_organization' => 'Editer l\'organisation',
-  'title_list_organization' => 'Liste des organisations',
-  'title_invite_users' => 'Inviter des utilisateurs dans l\'organisme',
-  'invite_users' => 'Inviter des utilisateurs dans l\'organisme',
-  'title_list_users' => 'Liste des utilisateurs',
-  'title_edit_user' => 'Editer l\'utilisateur',
+    'title_add_organization' => 'Ajouter une organisation',
+    'title_edit_organization' => 'Editer l\'organisation',
+    'title_list_organization' => 'Liste des organisations',
+    'title_invite_users' => 'Inviter des utilisateurs dans l\'organisme',
+    'invite_users' => 'Inviter des utilisateurs dans l\'organisme',
+    'title_list_users' => 'Liste des utilisateurs',
+    'title_edit_user' => 'Editer l\'utilisateur',
 
-  'login' => 'Se connecter ou créer un compte',
-  'register' => 'Créer un compte',
+    'login' => 'Se connecter ou créer un compte',
+    'register' => 'Créer un compte',
+    'wizard-profile' => 'Créez votre profil'
 ];
