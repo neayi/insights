8626e07343afb0027b53536130150c4e8c4c8f95
Guillaume COZIC <guillaume.cozic@gmail.com>
2021-03-30 14:31:29 +0200

    schedule import characteristics

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command('characteristics:import')->dailyAt('22:00');
     }
 
     /**
