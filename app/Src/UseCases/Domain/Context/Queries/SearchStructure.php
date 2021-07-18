cbbdeddac37a796bb46ec1455a047cfabdca5641
Guillaume COZIC <guillaume.cozic@gmail.com>
2021-07-18 11:43:53 +0200

    refacto / fix

--- app/Src/UseCases/Domain/Context/Queries/SearchStructure.php
+++ app/Src/UseCases/Domain/Context/Queries/SearchStructure.php
@@ -6,13 +6,20 @@ namespace App\Src\UseCases\Domain\Context\Queries;
 
 use GuzzleHttp\Client;
 
+/**
+ * Search structure remotely with the wiki api
+ */
 class SearchStructure
 {
     public function execute(string $search):array
     {
         $client = new Client();
         $uri = config('wiki.api_uri').'?action=query&list=search&srwhat=text&srsearch='.$search.'&srqiprofile=classic_noboostlinks&srnamespace=3000&format=json';
-        $response = $client->get($uri);
+        try {
+            $response = $client->get($uri);
+        }catch (\Throwable $e){
+            return ['results' => []];
+        }
         $content = json_decode($response->getBody()->getContents(), true);
         if(isset($content['query']['search'])){
             $results = array_column($content['query']['search'], 'title');
