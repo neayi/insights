4f3f69157d5e1a75cb241b3ff53879c0e4dc571e
Bertrand Gorge <bertrand.gorge@neayi.com>
2021-09-13 09:42:17 +0200

    Improved the look and feel of the email verify page

--- resources/views/public/auth/verify.blade.php
+++ resources/views/public/auth/verify.blade.php
@@ -11,10 +11,11 @@
     @endif
 
     <div class="col-lg-8 offset-lg-2 col-12 mt-lg-5" id="msg-err">
-        <p class="mb-4" style="color: red;">
+        <p class="mb-4  text-center " style="font-weight: bold">
             {{ __('adminlte::adminlte.verify_check_your_email') }}
-            {{ __('adminlte::adminlte.verify_if_not_recieved') }}
         </p>
+        
+        <p class="text-center"><img style="max-width:100%" src="{{asset('images/verify-email.png')}}" alt="" class=""/></p>
     </div>
 
 
@@ -22,9 +23,12 @@
         <div class="col-lg-8 offset-lg-2 col-12" id="msg-err">
             <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
                 @csrf
+                <p class="mb-4 text-center text-muted">
+                    {{ __('adminlte::adminlte.verify_if_not_recieved') }}
+                </p>
                 <div class="row">
-                    <div class="col-lg-3 offset-lg-2 col-md-4">
-                        <button type="submit" class="btn btn-dark-green text-white px-5 py-2 mr-2">
+                    <div class="text-center col-lg-5 col-md-7 col-10 mb-5 mx-auto">
+                        <button type="submit" class="btn btn-dark-green text-white px-5 py-2">
                             {{ __('adminlte::adminlte.verify_request_another') }}
                         </button>
                     </div>
