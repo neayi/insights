1f08cdc28728fee4330d296bbe0fc413766ab52f
guillaume <guillaume.cozic@gmail.com>
2021-11-30 17:52:01 +0100

    sync des utilisateurs

--- app/Console/Commands/SyncUsersDiscourse.php
+++ app/Console/Commands/SyncUsersDiscourse.php
@@ -9,6 +9,8 @@ use App\User;
 use GuzzleHttp\Client;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Str;
+
 
 class SyncUsersDiscourse extends Command
 {
@@ -23,31 +25,20 @@ class SyncUsersDiscourse extends Command
 
     public function handle()
     {
-        /*$sync = new UserSyncDiscourseModel();
-        $sync->user_id = 1;
-        $sync->uuid = 1;
-        $sync->sync = false;
-        $sync->save();
-        return;*/
-
         $hostname = config('services.discourse.api.url');
         $httpClient = new Client(['base_uri' => $hostname]);
 
-        UserSyncDiscourseModel::query()->where('sync', false)->chunkById(50, function ($items) use ($httpClient) {
+        UserSyncDiscourseModel::query()
+            //->where('sync', false)
+            ->chunkById(50, function ($items) use ($httpClient) {
             foreach($items as $userSync) {
                 $user = $userSync->user;
                 try {
-                    $this->createUserOnDiscourse($httpClient, $user);
-
-
+                    $id = $this->createUserOnDiscourse($httpClient, $user);
                     $userSync->sync = true;
                     $userSync->sync_at = (new \DateTime())->format('Y-m-d H:i:s');
                     $userSync->save();
-                    /*if(!$result){
-                        $message = 'No result from Api. Discourse sync failed for user : '.$user->uuid;
-                        $this->error($message);
-                        Log::emergency($message);
-                    }*/
+                    $this->uploadAvatar($httpClient, $user, $id);
                 }catch (\Throwable $e){
                     $message = 'Discourse sync failed for user : '.$user->uuid. ' '.$e->getMessage();
                     $this->error($message);
@@ -61,7 +52,6 @@ class SyncUsersDiscourse extends Command
     private function createUserOnDiscourse(Client $httpClient, User $user)
     {
         $apiKey = config('services.discourse.api.key');
-
         $result = $httpClient->post('users.json', [
             'headers' => [
                 'Api-Key' => $apiKey,
@@ -69,35 +59,50 @@ class SyncUsersDiscourse extends Command
                 'Content-Type' => 'application/json'
             ],
             'json' => [
-                'name' => $user->lastname,
-                'username' => $user->fullname(),
+                'username' => $this->user = substr(Str::of($user->fullname())->slug('-'), 0, 20),
                 'password' => uniqid().uniqid(),
                 'email' => $user->email,
             ]
         ]);
-        dd($result);
+        $result = json_decode($result->getBody()->getContents(), true);
+        if($result['success'] === false){
+            throw new \Exception($result['message']);
+        }
+        return $result['user_id'];
     }
 
-    private function uploadAvatar(Client $httpClient, User $user)
+    private function uploadAvatar(Client $httpClient, User $user, $id)
     {
         $apiKey = config('services.discourse.api.key');
-
         $result = $httpClient->post('uploads.json', [
             'headers' => [
                 'Api-Key' => $apiKey,
                 'Api-Username' => 'system',
-                'Content-Type' => 'application/json'
             ],
-            'json' => [
-                'type' => 'avatar',
-                'user_id' => $user->id,
-                'synchronous' => true,
-                'file' => $user->getAvatarUrlAttribute(),
+            'multipart' => [
+                [
+                    'name'     => 'type',
+                    'contents' => 'avatar',
+                ],
+                [
+                    'name'     => 'user_id',
+                    'contents' => $id,
+                ],
+                [
+                    'name'     => 'synchronous',
+                    'contents' => true,
+                ],
+                [
+                    'name'     => 'file',
+                    'contents' => fopen(storage_path($user->path_picture), 'r'),
+                ],
             ]
         ]);
 
+        $result = json_decode($result->getBody()->getContents(), true);
 
-        $uri = 'u/'.$user->fullname().'/preferences/avatar/pick.json';
+        $uploadId = $result['id'];
+        $uri = 'u/'.$this->user.'/preferences/avatar/pick.json';
         $httpClient->put($uri, [
             'headers' => [
                 'Api-Key' => $apiKey,
@@ -105,7 +110,7 @@ class SyncUsersDiscourse extends Command
                 'Content-Type' => 'application/json'
             ],
             'json' => [
-                'upload_id' => $result['upload_id'],
+                'upload_id' => $uploadId,
                 'type' => 'uploaded',
             ]
         ]);
