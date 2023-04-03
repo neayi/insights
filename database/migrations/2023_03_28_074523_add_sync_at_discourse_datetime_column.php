7f152552b366df4a4f02dd440ad3ba1a1555059b
GuillaumeCozic <guillaume.cozic@gmail.com>
2023-04-03 15:48:42 +0200

    fix issue 191 sync users into discourse from users table instead of user_sync_discourse (#196)

---
+++ database/migrations/2023_03_28_074523_add_sync_at_discourse_datetime_column.php
@@ -0,0 +1,38 @@
+<?php
+
+use App\Src\UseCases\Infra\Sql\Model\UserSyncDiscourseModel;
+use App\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddSyncAtDiscourseDatetimeColumn extends Migration
+{
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table){
+            $table->dateTime('sync_at_discourse')->nullable();
+        });
+
+        $this->migrateSyncAtDiscourse();
+    }
+
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table){
+            $table->dropColumn('sync_at_discourse');
+        });
+    }
+
+    public function migrateSyncAtDiscourse()
+    {
+        $usersSyncDiscourse = UserSyncDiscourseModel::query()
+            ->whereNotNull('sync_at')
+            ->get();
+        foreach($usersSyncDiscourse as $userSyncDiscourse) {
+            $user = User::query()->where('uuid', $userSyncDiscourse->uuid)->first();
+            $user->sync_at_discourse = $userSyncDiscourse->sync_at;
+            $user->save();
+        }
+    }
+}
