7441bac3c7082ce9035ac2cf3f7742c81560b02b
guillaume cozic <guillaume.cozic@gmail.com>
2023-07-25 09:33:15 +0200

    remove organization routes

--- routes/web.php
+++ routes/web.php
@@ -47,27 +47,9 @@ Route::group(['middleware' => ['auth', 'verified']], function() {
 });
 
 Route::group(['middleware' => ['auth', 'auth.check.role']], function() {
-    Route::get('/organizations', 'OrganizationsController@list')->name('organization.list');
-    Route::post('/organizations', 'OrganizationsController@listOrganizations')->name('organization.list.datatable');
-    Route::get('/organization/add/form', 'OrganizationsController@showAddForm')->name('organization.add.form');
-    Route::post('/organization/add', 'OrganizationsController@processAdd')->name('organization.add');
-    Route::get('/organization/{id}/edit/form', 'OrganizationsController@showEditForm')->name('organization.edit.form');
-    Route::post('/organization/{id}/edit', 'OrganizationsController@processEdit')->name('organization.edit');
-
-    Route::post('/organization/users/prepare-invite', 'OrganizationsController@prepareInvitation')->name('organization.users.prepare-invite');
-    Route::post('/organization/users/invite', 'OrganizationsController@sendInvitations')->name('organization.users.invite');
-
-    Route::get('/organization/{id}/users', 'UsersController@showListUsers')->name('users.list');
-    Route::post('/organization/{id}/users', 'UsersController@listUsers')->name('users.list.datatable');
-    Route::get('/organization/invite/accept', 'OrganizationsController@acceptInvite')->name('organization.invite.show');
-    Route::get('/organization/user/join', 'OrganizationsController@joinOrganization')->name('organization.user.join');
-
     Route::get('/user/{id}/edit/form', 'UsersController@editShowForm')->name('user.edit.form');
     Route::post('/user/{id}/edit', 'UsersController@editProcess')->name('user.edit');
     Route::post('/user/{id}/delete', 'UsersController@delete')->name('user.delete');
-    Route::post('/user/{id}/organization/{organization}/grant', 'UsersController@grantAsAdmin')->name('user.grant-admin.organization');
-    Route::post('/user/{id}/organization/{organization}/revoke', 'UsersController@revokeAsAdmin')->name('user.revoke-admin.organization');
-    Route::post('/user/{id}/organization/leave', 'UsersController@leaveOrganization')->name('user.leave.organization');
 
     Route::get('/user/edit/profile', 'ProfileController@showEditProfile')->name('user.edit.profile.show');
     Route::post('/user/edit/profile', 'ProfileController@processEditProfile')->name('user.edit.profile');
