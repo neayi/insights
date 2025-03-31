aa7803e986ecbe24ac1d52902f8500d66e5c4061
PéCé <pc.bertineau+gitlab@alterphp.com>
2025-03-31 18:35:52 +0200

    Fix load of form

--- resources/views/users/wizard-profile/fill-geolocation.blade.php
+++ resources/views/users/wizard-profile/fill-geolocation.blade.php
@@ -1,9 +1,11 @@
 @php
     $state = 'required';
-    if(old('country') && old('postal_code') && !$errors->has('country') && !$errors->has('postal_code')){
+    $country = $context['country'] ?? old('country');
+    $postalCode = $context['postal_code'] ?? old('postal_code');
+    if($country && $postalCode && !$errors->has('country') && !$errors->has('postal_code')){
         $state = 'success';
     }
-    if(old('country') > '' && old('postal_code') > '') {
+    if($country > '' && $postalCode > '') {
         $state = 'success';
     }
 @endphp
@@ -16,7 +18,7 @@
             <label>@lang('wiki_profile.fill_country')</label>
         </div>
         <div class="col-md-7 col-9">
-            <select value="{{old('country')}}" id="input-country" name="country"
+            <select value="{{ $country }}" id="input-country" name="country"
                     autocomplete="country" class="form-control" placeholder="" required>
                 @foreach(Countries::getList() as $countryCode => $countryName)
                     {{-- pré-sélection du pays en se basant sur la langue connue (peu fiable mais fait le job pour FR) --}}
@@ -32,7 +34,7 @@
             <label>@lang('wiki_profile.fill_postal_code')</label>
         </div>
         <div class="col-9 col-md-3">
-            <input value="{{old('postal_code')}}" type="text" id="input-postal-code" name="postal_code" autocomplete="postal-code" class="form-control" placeholder="" required>
+            <input value="{{$postalCode}}" type="text" id="input-postal-code" name="postal_code" autocomplete="postal-code" class="form-control" placeholder="" required>
             <input value="@if ('success' === $state) success @endif" type="text" style="height: 1px; opacity: 0; display: block" id="input-check-geolocation" title="Failed geolocation" name="check_geolocation" required>
         </div>
     </div>
