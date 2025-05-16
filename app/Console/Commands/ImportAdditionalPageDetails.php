42a52931c168f295d1da9c9878103d4cc122dcb8
alterphp <pc.bertineau@alterphp.com>
2025-05-16 10:57:39 +0200

    Mutualize WikiClient::ask method usage

--- app/Console/Commands/ImportAdditionalPageDetails.php
+++ app/Console/Commands/ImportAdditionalPageDetails.php
@@ -26,7 +26,9 @@ class ImportAdditionalPageDetails extends Command
             $this->info(sprintf("Importing Pages with icons and types from wiki %s", $localeConfig->code));
             $wikiClient = new WikiClient($localeConfig->toArray());
 
-            $content = $wikiClient->getPagesAdditionalDetail();
+            $baseQuery = '[[A un glyph::%2B]]|?A un glyph|?A un type de page';
+
+            $content = $wikiClient->ask($baseQuery);
             $pages = $content['query']['results'];
 
             $this->handlePages($pages, $wikiClient);
@@ -34,7 +36,7 @@ class ImportAdditionalPageDetails extends Command
 
             while ($continue !== null && $continue !== '') {
                 $this->info($continue);
-                $content = $wikiClient->getPagesAdditionalDetail($continue);
+                $content = $wikiClient->ask($baseQuery . '|offset=' . $continue);
                 $this->handlePages($pages);
                 $continue = $content['query-continue-offset'] ?? null;
             }
@@ -61,7 +63,7 @@ class ImportAdditionalPageDetails extends Command
                 $pageModel->icon = $icon;
             }
 
-            $pageModel->type = $typePage !== false ? $typePage : '';
+            $pageModel->type = $typePage ?? null;
             $pageModel->save();
         }
     }
