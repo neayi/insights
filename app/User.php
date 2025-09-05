64e291c43e55372586ef444a74df8ca3b8bc58be
alterphp <pc.bertineau@alterphp.com>
2025-09-05 12:02:15 +0200

    DROP users_sync_discourse table

--- app/User.php
+++ app/User.php
@@ -6,7 +6,6 @@ use App\Src\UseCases\Domain\Context\Dto\UserDto;
 use App\Src\UseCases\Infra\Sql\Model\CharacteristicsModel;
 use App\Src\UseCases\Infra\Sql\Model\ContextModel;
 use App\Src\UseCases\Infra\Sql\Model\UserCharacteristicsModel;
-use App\Src\UseCases\Infra\Sql\Model\UserSyncDiscourseModel;
 use Illuminate\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
@@ -176,18 +175,6 @@ class User extends Authenticatable implements \Illuminate\Contracts\Auth\MustVer
             $this->discourse_username,
             $this->path_picture === null
         );
-    }
-
-    public function askDiscourseSync()
-    {
-        $sync = UserSyncDiscourseModel::query()->where('user_id', $this->id)->first();
-        if(!isset($sync)){
-            $sync = new UserSyncDiscourseModel();
-            $sync->user_id = $this->id;
-            $sync->uuid = $this->uuid;
-        }
-        $sync->sync = false;
-        $sync->save();
     }
 
     /** Deprecated - please use locale()->wiki_url */
