6f49c41da19f7e0c9c3f4a845bb23465993dae1f
PéCé <pc.bertineau@alterphp.com>
2025-12-05 15:42:45 +0100

    Subscribes users to their profile characteristics page (#295)
    * Subscribes users to their profile characteristics page
    
    * Fixed the PR with the wiki col in Interactions
    
    ---------
    
    Co-authored-by: Bertrand Gorge <bertrand.gorge@neayi.com>

--- app/Console/Commands/UsersSubscribeCharacteristicsTags.php
+++ app/Console/Commands/UsersSubscribeCharacteristicsTags.php
@@ -4,6 +4,7 @@ namespace App\Console\Commands;
 
 use App\Src\UseCases\Domain\Forum\CharacteristicsForumSyncer;
 use App\Src\UseCases\Domain\Context\Model\Characteristic;
+use App\Src\UseCases\Infra\Sql\Model\InteractionModel;
 use Carbon\Carbon;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
@@ -22,7 +23,7 @@ class UsersSubscribeCharacteristicsTags extends Command
      *
      * @var string
      */
-    protected $description = 'Subscribes every users to the characteristics (forum) tags they have in their profile.';
+    protected $description = 'Subscribes users to the characteristics (forum) tags they have in their profile + follow matching page.';
 
     /**
      * Execute the console command.
@@ -40,8 +41,10 @@ class UsersSubscribeCharacteristicsTags extends Command
                 'characteristics.pretty_page_label AS char_label',
                 'characteristics.wiki AS locale',
             )
+            ->addSelect('pages.page_id AS characteristic_page_id')
             ->join('user_characteristics', 'user_characteristics.user_id', '=', 'u.id')
             ->join('characteristics', 'characteristics.id', '=', 'user_characteristics.characteristic_id')
+            ->join('pages', 'pages.page_id', '=', 'characteristics.page_id', 'left')
             ->whereIn('characteristics.type', [Characteristic::FARMING_TYPE, Characteristic::CROPPING_SYSTEM])
             ->where('user_characteristics.created_at', '>=', $dateThreshold->format('Y-m-d H:i:s'))
         ;
@@ -49,11 +52,29 @@ class UsersSubscribeCharacteristicsTags extends Command
         $users = $usersInfosQuery->get();
 
         foreach ($users->all() as $userRow) {
+            // Ensure the characteristic has a linked page
+            if (null !== $userRow->characteristic_page_id) {
+                $this->ensureUserFollowsCharacteristicPage(
+                    $userRow->user_id,
+                    $userRow->characteristic_page_id,
+                    $userRow->locale
+                );
+            }
+
             $forumSyncer->subscribeCharacteristicTagNotifications(
                 $userRow->user_id,
                 $userRow->locale,
                 $userRow->char_label ?? $userRow->char_title
             );
         }
+    }
+
+    private function ensureUserFollowsCharacteristicPage(int $userId, int $pageId, string $wiki): void
+    {
+        InteractionModel::upsert(
+            ['user_id' => $userId, 'page_id' => $pageId, 'follow' => true, 'wiki' => $wiki],
+            ['user_id', 'page_id'],
+            ['follow']
+        );
     }
 }
---
+++ database/migrations/2025_11_10_233029_unique_interaction_per_user_page.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        // Merge les interactions dupliquées (user_id + page_id)
+        DB::statement(<<<SQL
+            UPDATE interactions i1
+                INNER JOIN interactions i2 ON i1.id < i2.id AND i1.user_id = i2.user_id AND i1.page_id = i2.page_id AND i1.wiki = i2.wiki
+            SET i1.applause = GREATEST(i1.applause, i2.applause),
+                i1.follow = GREATEST(i1.follow, i2.follow),
+                i1.done = GREATEST(i1.done, i2.done),
+                i1.start_done_at = IFNULL(i1.start_done_at, i2.start_done_at),
+                i1.value = GREATEST(i1.value, i2.value)
+            WHERE i1.user_id IS NOT NULL;
+        SQL);
+        // Supprime les interactions en doublon
+        DB::statement(<<<SQL
+            DELETE i2
+            FROM interactions i1
+                INNER JOIN interactions i2 ON i1.id < i2.id AND i1.user_id = i2.user_id AND i1.page_id = i2.page_id AND i1.wiki = i2.wiki
+            WHERE i1.user_id IS NOT NULL;
+        SQL);
+        // Pose un index UNIQUE sur les interactions (user_id + page_id)
+        DB::statement(<<<SQL
+            CREATE UNIQUE INDEX unique_user_page ON interactions (user_id, page_id, wiki);
+        SQL);
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        DB::statement(<<<SQL
+            ALTER TABLE `interactions` DROP INDEX `unique_user_page`;
+        SQL);
+    }
+};
